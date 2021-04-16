e3f8bc353d93217c95b0dd30d9f0e2ab365dd63e
Kirillstrelbitskiy <strelOK042021@gmail.com>
2021-04-17 02:09:31 +0300

    Update

--- main.py
+++ main.py
@@ -1,69 +1,28 @@
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
-from PyQt5.QtGui import QPixmap
-import sys
+from PyQt5 import QtGui, QtCore, QtWidgets
 import cv2
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
-import numpy as np
+import sys
 
+class DisplayImageWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(DisplayImageWidget, self).__init__(parent)
 
-class VideoThread(QThread):
-    change_pixmap_signal = pyqtSignal(np.ndarray)
+        self.button = QtWidgets.QPushButton('Show picture')
+        self.button.clicked.connect(self.show_image)
+        self.image_frame = QtWidgets.QLabel()
 
-    def run(self):
-        # capture from web cam
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, cv_img = cap.read()
-            if ret:
-                self.change_pixmap_signal.emit(cv_img)
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.image_frame)
+        self.setLayout(self.layout)
 
+    @QtCore.pyqtSlot()
+    def show_image(self):
+        self.image = cv2.imread('placeholder4.PNG')
+        self.image = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+        self.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.image))
 
-class App(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Qt live label demo")
-        self.disply_width = 640
-        self.display_height = 480
-        # create the label that holds the image
-        self.image_label = QLabel(self)
-        self.image_label.resize(self.disply_width, self.display_height)
-        # create a text label
-        self.textLabel = QLabel('Webcam')
-
-        # create a vertical box layout and add the two labels
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.image_label)
-        vbox.addWidget(self.textLabel)
-        # set the vbox layout as the widgets layout
-        self.setLayout(vbox)
-
-        # create the video capture thread
-        self.thread = VideoThread()
-        # connect its signal to the update_image slot
-        self.thread.change_pixmap_signal.connect(self.update_image)
-        # start the thread
-        self.thread.start()
-
-
-
-    @pyqtSlot(np.ndarray)
-    def update_image(self, cv_img):
-        """Updates the image_label with a new opencv image"""
-        qt_img = self.convert_cv_qt(cv_img)
-        self.image_label.setPixmap(qt_img)
-    
-    def convert_cv_qt(self, cv_img):
-        """Convert from an opencv image to QPixmap"""
-        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
-        return QPixmap.fromImage(p)
-    
-if __name__=="__main__":
-    app = QApplication(sys.argv)
-    a = App()
-    a.show()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    display_image_widget = DisplayImageWidget()
+    display_image_widget.show()
     sys.exit(app.exec_())
