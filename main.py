bbd140dbedbb345e3577a0ba3e7a5b54ca622858
Kirillstrelbitskiy <strelOK042021@gmail.com>
2021-04-17 03:49:26 +0300

    Update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyvirtualcam import PixelFormat
 from PyQt5.QtWidgets import QMessageBox, QApplication, QLabel, QWidget, QPushButton, QVBoxLayout, QApplication, QGridLayout, QHBoxLayout, QMessageBox, QSlider
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import QThread, QTimer
+from PyQt5.QtCore import QThread, QTimer, Qt
 from imutils.perspective import four_point_transform
 from cv2 import cv2
 import numpy as np
@@ -29,43 +29,54 @@ prev_screenCnt = [[[]]]
 
 class UI_Window(QWidget):
     def __init__(self, camera = None):
-        super().__init__()
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.nextFrameSlot)
+            super().__init__()
+            self.timer = QTimer()
+            self.timer.timeout.connect(self.nextFrameSlot)
 
-        layout = QGridLayout()
-        layout.setSpacing(10)
+            layout = QGridLayout()
+            layout.setSpacing(10)
 
-        button_layout = QHBoxLayout()
+            button_layout = QHBoxLayout()
 
-        btnCamera = QPushButton("Open camera")
-        # btnCamera.clicked.connect(self.start)
-        button_layout.addWidget(btnCamera)
-        layout.addLayout(button_layout, 0, 0)
+            btnCamera = QPushButton("Open camera")
+            # btnCamera.clicked.connect(self.start)
+            button_layout.addWidget(btnCamera)
+            layout.addLayout(button_layout, 0, 0)
 
-        self.videoOrig = QLabel()
-        self.videoOrig.setFixedSize(widthImgSmall, heightImgSmall)
-        self.videoLines = QLabel()
-        self.videoLines.setFixedSize(widthImgSmall, heightImgSmall)
-        self.videoWraped = QLabel()
-        self.videoWraped.setFixedSize(widthImgSmall, heightImgSmall)
-        
-        layout.addWidget(self.videoOrig, 0, 1)
-        layout.addWidget(self.videoLines, 1, 1)
-        layout.addWidget(self.videoWraped, 2, 1)
+            self.videoOrig = QLabel()
+            self.videoOrig.setFixedSize(widthImgSmall, heightImgSmall)
+            self.videoLines = QLabel()
+            self.videoLines.setFixedSize(widthImgSmall, heightImgSmall)
+            self.videoWraped = QLabel()
+            self.videoWraped.setFixedSize(widthImgSmall, heightImgSmall)
+            
+            layout.addWidget(self.videoOrig, 0, 1)
+            layout.addWidget(self.videoLines, 1, 1)
+            layout.addWidget(self.videoWraped, 2, 1)
 
-        self.val1_sl = QSlider(Qt.Horizontal)
-        self.val1_sl.setMinimum(10)
-        self.val1_sl.setMaximum(30)
-        self.val1_sl.setValue(20)
-        self.val1_sl.setTickPosition(QSlider.TicksBelow)
-        self.val1_sl.setTickInterval(5)
+            self.val1_sl = QSlider(Qt.Horizontal)
+            self.val1_sl.setMinimum(0)
+            self.val1_sl.setMaximum(300)
+            self.val1_sl.setValue(60)
+            self.val1_sl.setTickPosition(QSlider.TicksBelow)
+            self.val1_sl.setTickInterval(2)
 
-        self.setLayout(layout)
-        self.setWindowTitle("First GUI with QT")
-        self.setFixedSize(widthImg, heightImg)
+            layout.addWidget(self.val1_sl, 1, 0)
 
-        self.timer.start(1000. / 24)
+            self.val2_sl = QSlider(Qt.Horizontal)
+            self.val2_sl.setMinimum(0)
+            self.val2_sl.setMaximum(300)
+            self.val2_sl.setValue(0)
+            self.val2_sl.setTickPosition(QSlider.TicksBelow)
+            self.val2_sl.setTickInterval(2)
+
+            layout.addWidget(self.val2_sl, 2, 0)
+
+            self.setLayout(layout)
+            self.setWindowTitle("First GUI with QT")
+            self.setFixedSize(widthImg, heightImg)
+
+            self.timer.start(1000. / 24)
 
     # def start(self):
         
@@ -87,7 +98,7 @@ class UI_Window(QWidget):
         gray = cv2.GaussianBlur(gray, (5, 5), 1)
 
         # val = helpers.valTrackbars()
-        val = [60, 0]
+        val = [self.val1_sl.value(), self.val2_sl.value()]
         edged = cv2.Canny(gray, val[0], val[1])
 
         edged_rsz = cv2.resize(edged, (widthImgSmall, heightImgSmall))
@@ -124,21 +135,20 @@ class UI_Window(QWidget):
             warped = four_point_transform(image_original, prev_screenCnt.reshape(4, 2))
             warped = warped[borders_width:warped.shape[0] - borders_width, borders_width:warped.shape[1] - borders_width]
             
-            warped = cv2.resize(warped, (widthImgSmall, heightImgSmall))
 
             # colors magic
-            # warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+            warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
             # T = threshold_local(warped, 11, offset = 10, method = "gaussian")
             # warped = (warped > T).astype("uint8") * 255
 
             # cv2.imshow("OO", image_original)
             # cv2.imshow("Warped", warped)
+            
+            with pyvirtualcam.Camera(widthImg, heightImg, 20, fmt=PixelFormat.BGR) as self.cam:
+                self.cam.send(warped)
+                self.cam.sleep_until_next_frame()
 
-            # cam.send(warped)
-            # cam.sleep_until_next_frame()
-
-        # rval, frame = camera.vc.read()
-        # frame = cv2.csvtColor(frame, cv2.COLOR_BGR2RGB)
+            warped = cv2.resize(warped, (widthImgSmall, heightImgSmall))
 
             img = QImage(warped, warped.shape[1], warped.shape[0], QImage.Format_RGB888).rgbSwapped()
             pixmap = QPixmap.fromImage(img)
